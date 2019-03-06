78a15dd8055b5162b840e1659c516019f38326ae
David <d.vanzessen@erasmusmc.nl>
2019-03-06 16:47:42 +0100

    added more queries

--- myFAIR/util.py
+++ myFAIR/util.py
@@ -3,8 +3,7 @@ import json
 class queries:
     @staticmethod
     def get_data_instance_query(
-        username, password, 
-        storage, 
+        username, password, storage, 
         title, 
         filename,
         content_type, 
@@ -12,32 +11,32 @@ class queries:
         projectid, 
         assayid, 
         description, 
-        tags)
+        tags):
 
         data = { 
             'data': { 
                 'type': 'data_files', 
-                'attributes': { 
+                'attributes': {
                     'title': title,
-                    'description': description},
+                    'description': description,
                     'tags': tags,
                     'license': 'CC-BY-4.0', 
-                    'content_blobs': [ {
+                    'content_blobs': [{
                         'original_filename': filename, 
-                        'content_type': content_type}
-                    }],
-                    'policy': {
-                        'access': 'download', 
-                        'permissions': [{ 
-                            'resource': { 
-                                'id': str(projectid),
-                                'type': 'projects'
-                            },
-                            'access': 'edit' 
-                        } ] 
-                    } 
-                }, 
-                'relationships': { 
+                        'content_type': content_type
+                    }]
+                },
+                'policy': {
+                    'access': 'download', 
+                    'permissions': [{ 
+                        'resource': { 
+                            'id': str(projectid),
+                            'type': 'projects'
+                        },
+                        'access': 'edit' 
+                    }]
+                },
+                'relationships': {
                     'creators': { 
                         'data': [ {
                             'id': str(userid),
@@ -56,7 +55,7 @@ class queries:
                             'type': 'assays' 
                         } ] 
                     } 
-                } 
+                }
             }
         }
         return "curl " +
@@ -66,5 +65,118 @@ class queries:
             username=username,
             password=password,
             storage=storage,
+            data=json.dumps(data)
+        )
+
+    @staticmethod
+    def get_study_creation_query(
+        username, password, server
+        title,
+        description,
+        userid,
+        projectid,
+        investigationid):
+
+        data = {
+            'type': 'studies',
+            'attributes': {
+                'title': title,
+                'description': description,
+                'person_responsible_id': str(userid),
+                'policy': {
+                    'access': 'download',
+                    'permissions': [{
+                        'resource': {
+                            'id': str(projectid),
+                            'type': 'projects'
+                        },
+                        'access': 'view'
+                    }]
+                }
+            },
+            'relationships': {
+                'investigation': {
+                    'data': {
+                        'id': str(investigationid),
+                        'type': 'investigations'
+                    }
+                },
+                'creators': {
+                    'data': [{
+                        'id': str(userid),
+                        'type': 'people'
+                    }]
+                }
+            }
+        }
+        return "curl " +
+            "-u {username}:{password} -X POST '{server}/studies' " +
+            "-H 'accept: application/json' -H 'Content-Type: application/json' " +
+            "-d '{data}' ".format(
+            username=username,
+            password=password,
+            server=server,
+            data=json.dumps(data)
+        )
+
+    @staticmethod
+    def get_assay_creation_query(
+        username, password,
+        title,
+        assay_type,
+        technology_type,
+        description,
+        projectid,
+        studyid
+        userid):
+        data = {
+            'data': {
+                'type': 'assays',
+                'attributes': {
+                    'title': title,
+                    'assay_class': {
+                        'key': 'EXP'
+                    },
+                    'assay_type': {
+                        'uri': assay_type
+                    },
+                    'technology_type': {
+                        'uri': technology_type
+                    },
+                    'description': description,
+                    'policy': {
+                        'access': 'download',
+                        'permissions': [{
+                            'resource': {
+                                'id': str(projectid),
+                                'type': 'projects' 
+                            },
+                            'access': 'view'
+                        }]
+                    }
+                }, 
+                'relationships': {
+                    'study': {
+                        'data': {
+                            'id': str(studyid),
+                            'type': 'studies'
+                        }
+                    },
+                    'creators': {
+                        'data': [{
+                            'id': str(userid),
+                            'type': 'people'
+                        }]
+                    }
+                }
+            }
+        }
+        return "curl " +
+            "-u {username}:{password} -X POST '{server}/assays' " +
+            "-H 'accept: application/json' -H 'Content-Type: application/json' " +
+            "-d '{data}' ".format(
+            username=username,
+            password=password,
+            server=server,
             data=json.dumps(data)
         )
