feb9159491b7cf4ce6a1f82141ee9a2bd327bd32
rjansen1984 <rick.jansen1984@gmail.com>
2019-09-24 17:02:38 +0200

    Cleaned DisGeNET SPARQL query

--- myFAIR/views.py
+++ myFAIR/views.py
@@ -2103,24 +2103,7 @@ def disgenet(disgenet):
     """
     disgenet_uri = {}
     sparql_query = (
-        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
-        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
-        "PREFIX owl: <http://www.w3.org/2002/07/owl#>" +
-        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>" +
         "PREFIX dcterms: <http://purl.org/dc/terms/>" +
-        "PREFIX foaf: <http://xmlns.com/foaf/0.1/>" +
-        "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" +
-        "PREFIX void: <http://rdfs.org/ns/void#>" +
-        "PREFIX sio: <http://semanticscience.org/resource/>" +
-        "PREFIX ncit: <http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#>" +
-        "PREFIX up: <http://purl.uniprot.org/core/>" +
-        "PREFIX dcat: <http://www.w3.org/ns/dcat#>" +
-        "PREFIX dctypes: <http://purl.org/dc/dcmitype/>" +
-        "PREFIX wi: <http://http://purl.org/ontology/wi/core#>" +
-        "PREFIX eco: <http://http://purl.obolibrary.org/obo/eco.owl#>" +
-        "PREFIX prov: <http://http://http://www.w3.org/ns/prov#>" +
-        "PREFIX pav: <http://http://http://purl.org/pav/>" +
-        "PREFIX obo: <http://purl.obolibrary.org/obo/>" +
         "SELECT * " +
         "WHERE { SERVICE <http://rdf.disgenet.org/sparql/> { " +
         "?uri dcterms:title ?disease . " +
